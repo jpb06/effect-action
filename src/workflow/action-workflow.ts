a98fe5b49d8ac449a839f46472bb4b8a2d3d28ac
jpb06 <jpb.06@outlook.com>
2024-12-10 22:43:30 +0100

    failure

--- src/workflow/action-workflow.ts
+++ src/workflow/action-workflow.ts
@@ -1,13 +1,25 @@
 import { Effect, pipe } from 'effect';
 import { runPromise } from 'effect-errors';
+import { TaggedError } from 'effect/Data';
 
 import { Console, ConsoleLive } from '@effects/console';
+
+export class CustomError extends TaggedError('custom-error')<{
+  cause?: unknown;
+  message?: string;
+}> {}
 
 const task = pipe(
   Effect.gen(function* () {
     const console = yield* Console;
 
     yield* console.info('yolo');
+
+    yield* Effect.fail(
+      new CustomError({
+        cause: 'Oh no',
+      }),
+    );
   }),
   Effect.withSpan('task'),
 );
